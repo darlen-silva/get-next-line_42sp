fd2326b49de06cb14f1988103460726b1b9e718f
darlen-silva <darlenn96@gmail.com>
2024-02-09 17:34:49 -0300

    Breaking 'string_from_read' into two functions.

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,34 @@
 /*   By: dardo-na <dardo-na@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 22:01:59 by dardo-na          #+#    #+#             */
-/*   Updated: 2024/02/08 22:51:14 by dardo-na         ###   ########.fr       */
+/*   Updated: 2024/02/09 17:30:28 by dardo-na         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
+
+static	size_t	read_line(int fd, char *buff, ssize_t *bytes, int *c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE && *c != '\n')
+	{
+		*bytes = read(fd, c, 1);
+		if (*bytes <= 0)
+			break ;
+		buff[i++] = *c;
+	}
+	buff[i] = '\0';
+	return (i);
+}
 
 static char	*string_from_read(int fd, char *str)
 {
 	char	*buff;
-	ssize_t	bytes;
-	size_t	i;
 	size_t	n;
 	int		c;
+	ssize_t	bytes;
 
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(*buff));
 	if (!buff)
@@ -30,17 +43,13 @@ static char	*string_from_read(int fd, char *str)
 	c = 0;
 	while (bytes > 0 && c != '\n')
 	{
-		i = 0;
-		while (i < BUFFER_SIZE && bytes > 0 && c != '\n')
+		n += read_line(fd, buff, &bytes, &c);
+		str = join_strings(str, buff, n);
+		if (str == NULL)
 		{
-			bytes = read(fd, &c, 1);
-			if (bytes <= 0)
-				break ;
-			buff[i++] = c;
-			n++;
+			free(buff);
+			return (NULL);
 		}
-		buff[i] = '\0';
-		str = join_strings(str, buff, n + i);
 	}
 	free(buff);
 	return (str);
@@ -60,20 +69,3 @@ char	*get_next_line(int fd)
 	str = NULL;
 	return (line);
 }
-/*
-int	main(void)
-{
-	char *buff;
-	int fd = open("./big.text", O_RDONLY);
-
-	while (1)
-	{
-		buff = get_next_line(fd);
-		if (buff == NULL)
-			break;
-		printf("%s", buff);
-		free(buff);
-	}
-	close(fd);
-	return (0);
-} */
